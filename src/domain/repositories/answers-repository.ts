7c702ce7c2fcb8e040b57dcc3e1f370cd0c31ee5
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-18 09:28:12 -0300

    feat: add answer repository

--- src/domain/repositories/answers-repository.ts
+++ src/domain/repositories/answers-repository.ts
@@ -1,6 +1,6 @@
-import { Answer } from "../entities/answer";
+import { Answer } from '../entities/answer'
 
-export interface AnswersRepository{
-    // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
-    create(answer: Answer): Promise<void>
+export interface AnswersRepository {
+  // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
+  create(answer: Answer): Promise<void>
 }
