2238e5418b2e040a9b0c696641a7f52029ac5c86
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-22 09:17:12 -0300

    feat: add get question by slug use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,27 @@
+import { Question } from '../../entrerprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findBySlug(slug)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    return { question }
+  }
+}
