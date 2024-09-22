c9702cd37e4bd2bc76da5b083281364bbbbedb13
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-22 09:16:45 -0300

    feat: add method findBySlug in contract repository and inmemoryrepository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -3,4 +3,5 @@ import { Question } from '../../entrerprise/entities/question'
 export interface QuestionsRepository {
   // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
   create(question: Question): Promise<void>
+  findBySlug(slug: string): Promise<Question | null>
 }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,6 +2,17 @@ import { Question } from '@/domain/forum/entrerprise/entities/question'
 import { QuestionsRepository } from './../../src/domain/forum/application/repositories/questions-repository'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
