4a06075217cf587b8eb055052f9ffea49aacc7ec
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-23 07:33:23 -0300

    feat: add methods findById and delete on repositories

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -3,4 +3,6 @@ import { Answer } from '../../entrerprise/entities/answer'
 export interface AnswersRepository {
   // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
   create(answer: Answer): Promise<void>
+  findById(answerId: string): Promise<Answer | null>
+  delete(answer: Answer): Promise<void>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -5,4 +5,20 @@ export class InMemoryAnswerRepository implements AnswersRepository {
   async create(answer: Answer) {
     this.items.push(answer)
   }
+
+  async findById(answerId: string) {
+    const answer = this.items.find((item) => item.id.toString() === answerId)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
 }
