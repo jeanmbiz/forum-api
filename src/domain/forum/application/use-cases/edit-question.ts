10b4dece105dcf8390dd002cf76f3e8b221b01d8
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-23 07:49:38 -0300

    feat: add edit question use case

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,40 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface EditQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  title: string
+  content: string
+}
+
+interface EditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not Allowed.')
+    }
+
+    // edita o titulo e conteudo da pergunta
+    question.title = title
+    question.content = content
+
+    await this.questionsRepository.save(question)
+
+    return {}
+  }
+}
