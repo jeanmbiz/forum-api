00606782763ef1427451df6c69f35e8ea953a741
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-23 07:18:08 -0300

    feat: add delete use case

---
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -0,0 +1,32 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface DeleteQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+}
+
+interface DeleteQuestionUseCaseResponse {}
+
+export class DeleteQuestionUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not Allowed.')
+    }
+
+    await this.questionsRepository.delete(question)
+
+    return {}
+  }
+}
