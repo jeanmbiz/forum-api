e8adf264e5651493cb3e202153ca855ef76c4428
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-24 07:19:49 -0300

    feat: add choose question best answer use case

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,51 @@
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Question } from '../../entrerprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private answersRepository: AnswersRepository,
+  ) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    // adiciona no bestAnswerId o id da resposta
+    question.bestAnswerId = answer.id
+
+    // salva a pergunta no questionsRepository
+    await this.questionsRepository.save(question)
+
+    return { question }
+  }
+}
