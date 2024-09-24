4f484332909d26be41e3b36413ad197bb944c892
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-24 08:02:42 -0300

    feat: add fetch recent question use case

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,23 @@
+import { Question } from '../../entrerprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+
+    return { questions }
+  }
+}
