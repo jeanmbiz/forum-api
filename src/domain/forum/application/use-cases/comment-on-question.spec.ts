57ed88eb43178e4bfe1731868132986fead1f9ac
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-25 11:07:15 -0300

    test: add comment on question use case test

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+// sut = system under test
+let sut: CommentOnQuestionUseCase
+
+describe('Choose Question Best Answer', () => {
+  beforeEach(() => {
+    // automatiza a criação do DB em memória e use case
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+  it('should be able to comment on question', async () => {
+    // criar question pelo Factory
+    const question = makeQuestion()
+
+    // salva question e answer nos repositórios
+    await inMemoryQuestionsRepository.create(question)
+
+    // executa o caso de uso
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comentário teste',
+    })
+
+    // espero que no repositório o content seja igual a 'Comentário teste'.
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
