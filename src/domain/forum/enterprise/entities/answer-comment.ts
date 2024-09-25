101c073dc9de84bd97f6197dc302245114a25067
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-25 08:13:40 -0300

    refactor: to extends Comment class

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,40 +1,17 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityID
+// extende CommentProps e customiza para respostas dos comentários
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
-export class Answer extends Entity<AnswerCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
 
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  // faz atualizaçao do upatedAt
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  // atualização do comentário
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+// Extende a Classe comment
+export class Answer extends Comment<AnswerCommentProps> {
+  // getter da interface customizada
+  get answerId() {
+    return this.props.answerId
   }
 
   // abstrair a criação das entidades
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,40 +1,17 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+// extende CommentProps e customiza para perguntas dos comentários
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
-export class Question extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
 
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  // faz atualizaçao do upatedAt
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  // atualização do comentário
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+// Extende a Classe comment
+export class Question extends Comment<QuestionCommentProps> {
+  // getter da interface customizada
+  get questionId() {
+    return this.props.questionId
   }
 
   // abstrair a criação das entidades
