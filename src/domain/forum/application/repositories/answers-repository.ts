37527fc7270a444f5782d51d0c197ba701fd3a25
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-25 06:32:52 -0300

    feat: add method findManyByQuestionId in repositories

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
@@ -5,5 +6,9 @@ export interface AnswersRepository {
   create(answer: Answer): Promise<void>
   save(answer: Answer): Promise<void>
   findById(answerId: string): Promise<Answer | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<Answer[]>
   delete(answer: Answer): Promise<void>
 }
