c0ad821debdd679c7d5abb254a59adbb927069c8
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-27 07:31:11 -0300

    feat: add methods of findById and delete on repositories

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -3,4 +3,6 @@ import { AnswerComment } from '../../enterprise/entities/answer-comment'
 export interface AnswerCommentsRepository {
   // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
   create(answerComment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
+  delete(answerComment: AnswerComment): Promise<void>
 }
