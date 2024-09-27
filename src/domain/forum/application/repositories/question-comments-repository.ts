d866dfa03dedc9d457ec1aca28a128659b9aaaef
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-27 07:17:24 -0300

    feat: add methods in repository and inMemoryRepository

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -3,4 +3,6 @@ import { QuestionComment } from '../../enterprise/entities/question-comment'
 export interface QuestionCommentsRepository {
   // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
   create(questionComment: QuestionComment): Promise<void>
+  findById(id: string): Promise<QuestionComment | null>
+  delete(questionComment: QuestionComment): Promise<void>
 }
