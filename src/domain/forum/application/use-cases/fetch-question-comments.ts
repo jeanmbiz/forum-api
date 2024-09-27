47910572d8d8a7869824f021be294d6f58025d64
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-27 07:58:34 -0300

    feat: add fetch question comments use case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,32 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  // dependência do repositody - contrato/interface
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(
+        questionId,
+        // objeto abaixo é para metadados: paginação, filtro, ordenação etc
+        {
+          page,
+        },
+      )
+
+    return { questionComments }
+  }
+}
