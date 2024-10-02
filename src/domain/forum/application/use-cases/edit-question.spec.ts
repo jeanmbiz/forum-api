12890515b04bc0975d52b1c6d25d2dd226903fe5
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-02 08:04:18 -0300

    test: add Question Attachment in use case test

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -1,10 +1,13 @@
+import { InMemoryQuestionAttachmentsRepository } from './../../../../../test/repositories/in-memory-question-attachments-repository'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryQuestionsRepository } from '../../../../../test/repositories/in-memory-questions-repository'
 import { EditQuestionUseCase } from './edit-question'
 import { makeQuestion } from 'test/factories/make-question'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachments'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 // sut = system under test
 let sut: EditQuestionUseCase
 
@@ -12,7 +15,13 @@ describe('Edit Question', () => {
   beforeEach(() => {
     // automatiza a criação do DB em memória e use case
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+
+    sut = new EditQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionAttachmentsRepository,
+    )
   })
   it('should be able to edit a question by id', async () => {
     // criar pergunta c/ id pelo Factory
@@ -26,17 +35,42 @@ describe('Edit Question', () => {
     // salvo pergunta no repositório
     await inMemoryQuestionsRepository.create(newQuestion)
 
+    // cadastro 2 anexos para a pergunta criada
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    )
+
     await sut.execute({
       questionId: newQuestion.id.toValue(),
       authorId: 'author-1',
       title: 'Question test',
       content: 'Content test',
+      // passando nova lista de ids de anexos para edição
+      attachmentsIds: ['1', '3'],
     })
 
     expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
       title: 'Question test',
       content: 'Content test',
     })
+    // certificar que tem 2 anexos no repositório
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    // certificar que no repositório tem 1 array com 2 com attachmentsIds 1 e 3
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('3') }),
+    ])
   })
 
   it('should not be able to edit a question by id from another author', async () => {
@@ -56,6 +90,7 @@ describe('Edit Question', () => {
       authorId: 'author-2',
       title: 'Question test',
       content: 'Content test',
+      attachmentsIds: [],
     })
 
     // espero que seja erro
