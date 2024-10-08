45d01b29f1f6aa68b7d32f7e2846a8891f6b7070
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-08 08:47:45 -0300

    feat: add methods in repositories

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -9,4 +9,24 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.items.push(notification)
   }
+
+  async save(notification: Notification) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.items[itemIndex] = notification
+  }
+
+  async findById(notificationId: string) {
+    const notification = this.items.find(
+      (item) => item.id.toString() === notificationId,
+    )
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
 }
